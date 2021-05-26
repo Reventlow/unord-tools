9cb9e7b9171a1ba4f88e54cd14285c71c9046889
Reventlow <gorm@reventlow.com>
2021-05-26 12:55:58 +0200

    asset_detail.html model_hardware typo

--- UnordToolsProject/settings.py
+++ UnordToolsProject/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'unord-tools.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'unord-tools.herokuapp.com']
 
 
 # Application definition
