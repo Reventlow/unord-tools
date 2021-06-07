58ba9358e68289dc18cc9178bf32153dc98dca18
Reventlow <gorm@reventlow.com>
2021-06-07 15:24:06 +0200

    authentication

--- asset_app/views.py
+++ asset_app/views.py
@@ -12,7 +12,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
 #test
-@method_decorator(login_required, login_url='login')
+@method_decorator(login_required, name='dispatch')
 class AssetListView(generic.ListView):
     model = models.Asset
     form_class = forms.AssetForm
