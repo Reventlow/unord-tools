48b2a6190371183bdace776a5d91a34c26ea464c
Reventlow <gorm@reventlow.com>
2021-06-16 08:14:09 +0200

    1-2-1 jobs

--- asset_app/views.py
+++ asset_app/views.py
@@ -374,7 +374,7 @@ class One2OneInfoListView(generic.ListView):
 
     def get_queryset(self):
         queryset = super().get_queryset().order_by('name')
-        qs = queryset.annotate(object_count=Count('one2OneInfoLog'))
+        qs = queryset.annotate(object_count=Count('one2oneInfoLog'))
         return qs
 
     def completed_false(request, res_id):
