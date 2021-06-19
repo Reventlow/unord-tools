c2d1de00b33302e89b28a874f059ae43b72ed984
Gorm Reventlow <gorm@reventlow.com>
2021-06-19 08:05:52 +0200

    Case system update

--- asset_app/views.py
+++ asset_app/views.py
@@ -274,7 +274,7 @@ class Dashboard(generic.TemplateView):
             room_type__name='Afdeling').order_by('last_inspected', 'location', 'name')
         context['assetCases'] = models.AssetCase.objects.exclude(solved='True').order_by('created')
         context['bundelReservations'] = models.Bundle_reservation.objects.filter(returned=False, return_date > context_entry_inspection_time).order_by('return_date')
-        context['loan_assets'] = models.Loan_asset.objects.filter(returned=False).order_by('return_date')
+        context['loan_assets'] = models.Loan_asset.objects.filter(returned=False, return_date > context_entry_inspection_time).order_by('return_date')
         context['routinelogs'] = models.RoutineLog.objects.all().order_by('routine__name', '-date').distinct(
             'routine__name')
         context['one2ones'] = models.One2OneInfo.objects.filter(completed=False).annotate(
