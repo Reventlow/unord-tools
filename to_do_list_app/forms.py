064a614c5ff3afa50480a0c14c0060c2a2e3c891
Gorm Reventlow <gorm@reventlow.com>
2021-06-19 14:42:33 +0200

    to-do-list update

--- to_do_list_app/forms.py
+++ to_do_list_app/forms.py
@@ -3,6 +3,12 @@ from .models import Jobs
 
 
 class JobsForm(forms.ModelForm):
+    item = forms.CharField(label="", max_length=100, widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Opgave beskrivelse'}))
+    to_do_owner = forms.ModelChoiceField(queryset=Jobs.objects.all().order_by('-sl_level'),
+                                            label="Hvem er ansvarlige for opgaven",
+                                            widget=forms.Select(attrs={'class': 'form-control'}))
+    completed = forms.BooleanField(label="Er opgaven løst", initial=False, required=False)
 
     class Meta:
         model = Jobs
