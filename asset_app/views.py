c92d7affb2ba0009a01f39e03f82439b2389ad6f
Gorm Reventlow <gorm@reventlow.com>
2021-08-03 21:24:54 +0200

    export to excel implmentation

--- asset_app/views.py
+++ asset_app/views.py
@@ -563,7 +563,7 @@ class LocationLaptopListView(generic.ListView):
             row = 3 + idx
             worksheet_s.write_number(row, 0, idx + 1)
             worksheet_s.write_string(row, 1, data.name)
-            worksheet_s.write_string(row, 2, data.room)
+            worksheet_s.write_string(row, 2, data.room.name)
             worksheet_s.write_string(row, 3, data.model_hardware.asset_type)
             worksheet_s.write_string(row, 4, data.serial)
             worksheet_s.write_string(row, 4, data.may_be_loaned)
