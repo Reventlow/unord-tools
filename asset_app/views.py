04170b7996fbde28a06bcdd2d0d6dcfa5aeda2cb
Gorm Reventlow <gorm@reventlow.com>
2021-08-05 23:46:33 +0200

    export to excel implmentation

--- asset_app/views.py
+++ asset_app/views.py
@@ -884,7 +884,10 @@ class RoomDetailExcelView(generic.DetailView):
 
         worksheet_s.write_string(5, thisColumn, datetime.datetime.strptime(str(last_inspected), '%Y-%m-%d').strftime('%m/%d/%Y'))
 
-        thisRow = 18
+        if image:
+            thisRow = 23
+        else:
+            thisRow = 13
 
         worksheet_s.write(thisRow, 1, ugettext("Navn"), header)
         worksheet_s.write(thisRow, 2, ugettext("Mærke og model"), header)
