020bbb3269ec3d3e776604f30c10ef20bd3cf206
Gorm Reventlow <gorm@reventlow.com>
2021-09-09 18:24:30 +0200

    Home pageupdate

--- pages_app/views.py
+++ pages_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+import to_do_list_app
+from to_do_list_app import models
 
 @login_required(login_url='login')
 def home(request):
-    context = super().get_context_data(**kwargs)
-    context['to_dos'] = to_do_list_app.models.Jobs.objects.filter(completed=False).order_by('created')
+    to_dos = to_do_list_app.models.Jobs.objects.filter(completed=False).order_by('created')
+    context = {'to_dos' : to_dos}
     return render(request, 'home.html', context)
 
 
