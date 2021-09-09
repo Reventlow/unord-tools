d19025dd8b86cec4f371a70568348256f2034a49
Gorm Reventlow <gorm@reventlow.com>
2021-09-09 18:14:48 +0200

    Home pageupdate

--- asset_app/views.py
+++ asset_app/views.py
@@ -735,23 +735,20 @@ class Loan_assetCreateView(generic.CreateView):
             # asset.save()  # save entire instance, overwriting `last_updated` OR
             asset.save(update_fields=["is_loaned"])  # updates only `is_loaned
 
-            if str.isdecimal(form.cleaned_data['loaner_telephone_number']) and form.cleaned_data['loaner_type'] == 1:
-
-            url = "https://api.sms.dk/v1/sms/send"
-
-            payload = json.dumps({
-                "receiver": forms.Loan_assetForm.clean_loaner_telephone_number(),
-                "senderName": "U/Nord IT",
-                "message": ", vi har registreret at du har lånt følgende PC: "+ form.cleaned_data + ", fra ",
-                "format": "gsm",
-                "encoding": "utf8",
-            })
-            headers = {
-                'Authorization': 'Bearer Your-API-key',
-                'Content-Type': 'application/json'
-            }
-
-            response = requests.request("POST", url, headers=headers, data=payload)
+            #if str.isdecimal(form.cleaned_data['loaner_telephone_number']) and form.cleaned_data['loaner_type'] == 1:
+               #url = "https://api.sms.dk/v1/sms/send"
+               #payload = json.dumps({
+            #    "receiver": forms.Loan_assetForm.clean_loaner_telephone_number(),
+            #    "senderName": "U/Nord IT",
+            #    "message": ", vi har registreret at du har lånt følgende PC: "+ form.cleaned_data + ", fra ",
+            #    "format": "gsm",
+            #    "encoding": "utf8",
+          #  })
+               #headers = {
+               # 'Authorization': 'Bearer Your-API-key',
+               # 'Content-Type': 'application/json'
+            #}
+               #response = requests.request("POST", url, headers=headers, data=payload)
 
 
 
--- pages_app/views.py
+++ pages_app/views.py
@@ -3,12 +3,10 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='login')
 def home(request):
-      return render(request, 'home.html', {})
+    context = super().get_context_data(**kwargs)
+    context['to_dos'] = to_do_list_app.models.Jobs.objects.filter(completed=False).order_by('created')
+    return render(request, 'home.html', context)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['to_dos'] = to_do_list_app.models.Jobs.objects.filter(completed=False).order_by('created')
-        return context
 
 @login_required(login_url='login')
 def about(request):
