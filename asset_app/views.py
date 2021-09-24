02ac7dc49b17dbb0d8b8929239788041e32fa174
Reventlow <gorm@reventlow.com>
2021-09-24 13:27:04 +0200

    Update to loan asset filter view

--- asset_app/views.py
+++ asset_app/views.py
@@ -1087,12 +1087,11 @@ class Loan_assetListExcelView(generic.DetailView):
                 worksheet_s.write_string(row, 3, data.loaner_type.name, formatGreen)
                 worksheet_s.write_string(row, 4, data.loaner_telephone_number, formatGreen)
                 worksheet_s.write_string(row, 5, data.loaner_email, formatGreen)
-                worksheet_s.write_string(row, 6, data.loaner_address, formatGreen)
-                worksheet_s.write_string(row, 7, data.asset.name, formatGreen)
-                worksheet_s.write_string(row, 8, data.asset.model_hardware.asset_type.name, formatGreen)
-                worksheet_s.write_string(row, 9, data.asset.model_hardware.brand.name+' '+ data.asset.model_hardware.name, formatGreen)
-                worksheet_s.write_string(row, 10, datetime.datetime.strptime(str(data.loan_date), '%Y-%m-%d').strftime('%d/%m/%Y'), formatGreen)
-                worksheet_s.write_string(row, 11, datetime.datetime.strptime(str(data.return_date), '%Y-%m-%d').strftime('%d/%m/%Y'), formatGreen)
+                worksheet_s.write_string(row, 6, data.asset.name, formatGreen)
+                worksheet_s.write_string(row, 7, data.asset.model_hardware.asset_type.name, formatGreen)
+                worksheet_s.write_string(row, 8, data.asset.model_hardware.brand.name+' '+ data.asset.model_hardware.name, formatGreen)
+                worksheet_s.write_string(row, 9, datetime.datetime.strptime(str(data.loan_date), '%Y-%m-%d').strftime('%d/%m/%Y'), formatGreen)
+                worksheet_s.write_string(row, 10, datetime.datetime.strptime(str(data.return_date), '%Y-%m-%d').strftime('%d/%m/%Y'), formatGreen)
                 if data.returned == True:
                     returnedValue = "Ja"
                 else:
