48d79230c28ad441f5c3ac76b35a798a9b523655
Reventlow <gorm@reventlow.com>
2021-09-24 14:39:02 +0200

    Update to loan asset filter view

--- asset_app/views.py
+++ asset_app/views.py
@@ -1071,7 +1071,7 @@ class Loan_assetListExcelView(generic.DetailView):
                     worksheet_s.write_string(row, 2, data.location.name, formatRed)
                     worksheet_s.write_string(row, 3, data.loaner_type.name, formatRed)
                     worksheet_s.write_string(row, 4, data.loaner_telephone_number, formatRed)
-                    worksheet_s.write_boolean(row, 5, data.loaner_email, formatRed)
+                    worksheet_s.write_string(row, 5, data.loaner_email, formatRed)
                     if data.asset.name is not None:
                         worksheet_s.write_string(row, 6, data.asset.name, formatRed)
                     worksheet_s.write_string(row, 7, data.asset.model_hardware.asset_type.name, formatRed)
