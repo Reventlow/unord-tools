eaf6b541adda7948043923cb86c8b4297307c02f
Reventlow <gorm@reventlow.com>
2021-09-30 18:15:49 +0200

    Implementation of SMS funcionality

--- asset_app/clock.py
+++ asset_app/clock.py
@@ -1,6 +1,13 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from tools import smsAutoLateReturn, smsAutoReturnReminder, smsAutoLoanAsset
 
+TINYMCE_DEFAULT_CONFIG = {
+    "plugins": "advlist,autolink,lists,link,image,charmap,print,preview,anchor,searchreplace,visualblocks,code,fullscreen,insertdatetime,media,table,paste,",# plugins,
+    "toolbar": "styleselect |undo redo | bold italic | alignleft aligncenter alignright | link image media codesample code", # toolbar
+    "height": 500,  # texteditor height
+
+}
+
 sched = BlockingScheduler()
 
 @sched.scheduled_job('interval', minutes=3)
