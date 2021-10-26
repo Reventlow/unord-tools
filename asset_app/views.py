c20ee06a73cbee93ade469115d6943dd385f5588
Reventlow <gorm@reventlow.com>
2021-10-26 15:47:08 +0200

    asset_loan_filter excel list updated

--- asset_app/views.py
+++ asset_app/views.py
@@ -908,7 +908,7 @@ class Loan_assetListView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         new_context_entry = datetime.date.today()
-        context["loc_name"] = self.request.GET.get('loc_name')
+        context["loc_name"] = self.kwargs('loc_name')
         context["return_date"] = kwargs.get('return_date')
         context["returned"] = kwargs.get('returned')
         context["task"] = kwargs.get('task')
