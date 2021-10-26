3dc9dcebfa63d5903ec54ef27eddbc8ba7365595
Reventlow <gorm@reventlow.com>
2021-10-26 14:49:49 +0200

    asset_loan_filter excel list updated

--- asset_app/views.py
+++ asset_app/views.py
@@ -1051,7 +1051,7 @@ class Loan_assetListExcelView(generic.DetailView):
 
         thisRow = thisRow +1
         queryset = models.Loan_asset.objects.all().order_by('returned', 'loaner_name', 'asset')
-
+"""
         for idx, data in enumerate(queryset):
             row = thisRow + idx
 
@@ -1126,7 +1126,7 @@ class Loan_assetListExcelView(generic.DetailView):
 
             if not data.asset:
                 thisRow = thisRow - 1
-
+"""
             # the rest of the data
 
         worksheet_s.set_column('B:B', 30)
