fad7fd2e668006b1e812fe4af04f65da4cc3a331
Gorm Reventlow <gorm@reventlow.com>
2023-08-28 12:38:46 +0200

    adding asset api

--- asset_app/urls.py
+++ asset_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
-#from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 from django.conf.urls.static import settings, static
 from . import api
 from . import views
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.routers import DefaultRouter
+
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -27,8 +27,7 @@ urlpatterns = [
     path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/', include(router.urls)),
-    path('', views.AssetViewAPI.as_view(), name='list'),
-    path('', views.Loan_assetViewAPI.as_view(), name='list'),
+    path('api/', include(router.urls)),
 #Asset
     path("asset_app/asset/create/", views.AssetCreateView.as_view(), name="asset_app_asset_create"),
     path("asset_app/asset/<str:location>/", views.AssetListView.as_view(), name="asset_app_asset_list"),
