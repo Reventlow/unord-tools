47d87d4ee4a4749c1758aa77377e2fb959be7eaa
Gorm Reventlow <gorm@reventlow.com>
2023-08-28 13:05:40 +0200

    adding asset api

--- UnordToolsProject/settings.py
+++ UnordToolsProject/settings.py
@@ -152,7 +152,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ]
 }
-}
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
